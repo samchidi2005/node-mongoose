4fa24138e3a05aefdd778719d41ddd223010b9a4
Chidiebere Nwabunike <samchidi2005@yahoo.com>
2020-06-04 12:24:26 -0500

    Mongoose Part 3

--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
 const campsiteSchema = new Schema({
     name: {
         type: String,
@@ -11,6 +30,7 @@ const campsiteSchema = new Schema({
         type: String,
         required: true
     },
+    comments: [commentSchema]
 }, {
     timestamps: true 
 });
